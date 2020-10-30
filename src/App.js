289071a2b4c48bd7f768cdba3953a73cd77b62eb
parang <appdler96@gmail.com>
2020-10-31 08:13:56 +0900

    STEP2: Github User API 연결하기

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from "react";
 import "./App.css";
 import SearchInput from "./components/SearchInput";
 import SearchResult from "./components/SearchResult";
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "https://api.github.com/users/",
+});
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  const getUserAPI = async (username) => {
+    const { data } = await client.get(username);
+    console.log("data", data);
+
+    setUser(data);
+  };
+
+  React.useEffect(() => {
+    getUserAPI("pa-rang");
+  }, []);
+
+  console.log("user", user);
+
   return (
     <>
       <SearchInput />
